19c97a7407f429ff7475ee607e5eba049ae21922
Anjani Kumar <99578509+ANJani9web@users.noreply.github.com>
2023-01-30 10:20:36 +0000

    Done

---
+++ index.js
@@ -0,0 +1,25 @@
+// There are 7 types of data types in JS
+// NN BB SS U
+// Null Number Boolean Bigint String Symbol Undefined
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt(456);
+let e = "Anjani kumar";
+let f = Symbol("# is a symbol");
+let g = undefined;
+
+console.log(a,b,c,d,e,f,g);
+
+// Non Primitive Data types - OBJECTS in JS
+
+const item = {
+  "Harry":true,
+  "Code": false,
+  "Number": 456
+}
+
+console.log("\n");
+console.log(item);
+console.log(item["Code"]);
